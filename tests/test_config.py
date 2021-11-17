a2b9ca053ba0cdc301c22b207ed083f4d7e8cbf3
Akuli <akuviljanen17@gmail.com>
2021-11-17 16:36:24 +0200

    delete old tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -42,7 +42,7 @@ def reconnect_with_change(server_view, mocker, key, old, new):
     assert new_config[key] == old
     new_config[key] = new
     mocker.patch(
-        "irc_client.config.show_server_config_dialog"
+        "irc_client.config.show_connection_settings_dialog"
     ).return_value = new_config
     server_view.show_config_dialog()
 
@@ -55,27 +55,3 @@ def test_changing_host(alice, mocker, wait_until):
     assert alice.view_selector.item(server_view.view_id, "text") == "127.0.0.1"
     assert (alice.log_dir / "localhost" / "#autojoin.log").exists()
     assert (alice.log_dir / "127.0.0.1" / "#autojoin.log").exists()
-
-
-def test_changing_autojoins(alice, mocker, wait_until):
-    alice.entry.insert("end", "/join #lol")
-    alice.on_enter_pressed()
-    wait_until(lambda: "The topic of #lol is:" in alice.text())
-
-    server_view = alice.get_server_views()[0]
-    reconnect_with_change(
-        server_view,
-        mocker,
-        "joined_channels",
-        old=["#autojoin", "#lol"],
-        new=["#autojoin"],
-    )
-    wait_until(lambda: alice.text().count("The topic of #autojoin is:") == 2)
-    assert server_view.find_channel("#lol") is None
-
-
-def test_changing_nick_in_setting_dialog(alice, mocker, wait_until):
-    server_view = alice.get_server_views()[0]
-    reconnect_with_change(server_view, mocker, "nick", old="Alice", new="Anna")
-    wait_until(lambda: alice.text().count("The topic of #autojoin is:") == 2)
-    assert alice.nickbutton["text"] == "Anna"
