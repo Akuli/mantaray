39f253815b516c0684f3255a4c888c110aaab63b
Akuli <akuviljanen17@gmail.com>
2021-12-03 11:51:42 +0200

    Revert "TEMPORARILY delete most tests"
    This reverts commit 537ac0b0b9feea987bc8f2469835721297c44ab7.

---
+++ tests/test_config.py
@@ -0,0 +1,171 @@
+import copy
+from tkinter import ttk
+from tkinter.font import Font
+
+from mantaray.config import load_from_file, show_connection_settings_dialog
+
+
+def test_old_config_format(tmp_path, root_window):
+    (tmp_path / "config.json").write_text(
+        """
+        {
+          "servers": [
+            {
+              "host": "irc.libera.chat",
+              "port": 6697,
+              "nick": "Akuli2",
+              "username": "Akuli2",
+              "realname": "Akuli2",
+              "joined_channels": [
+                "##learnpython"
+              ]
+            }
+          ]
+        }
+        """
+    )
+    assert load_from_file(tmp_path) == {
+        "servers": [
+            {
+                "host": "irc.libera.chat",
+                "port": 6697,
+                "ssl": True,
+                "nick": "Akuli2",
+                "username": "Akuli2",
+                "realname": "Akuli2",
+                "password": None,
+                "joined_channels": ["##learnpython"],
+                "extra_notifications": [],
+                "join_leave_hiding": {"show_by_default": True, "exception_nicks": []},
+            }
+        ],
+        "font_family": Font(name="TkFixedFont", exists=True)["family"],
+        "font_size": Font(name="TkFixedFont", exists=True)["size"],
+    }
+
+
+def reconnect_with_change(server_view, mocker, key, old, new):
+    new_config = server_view.get_current_config().copy()
+    assert new_config[key] == old
+    new_config[key] = new
+    mocker.patch(
+        "mantaray.config.show_connection_settings_dialog"
+    ).return_value = new_config
+    server_view.show_config_dialog()
+
+
+def test_changing_host(alice, mocker, wait_until):
+    server_view = alice.get_server_views()[0]
+    reconnect_with_change(server_view, mocker, "host", old="localhost", new="127.0.0.1")
+    wait_until(lambda: alice.text().count("The topic of #autojoin is:") == 2)
+
+    assert alice.view_selector.item(server_view.view_id, "text") == "127.0.0.1"
+    assert (alice.log_dir / "localhost" / "#autojoin.log").exists()
+    assert (alice.log_dir / "127.0.0.1" / "#autojoin.log").exists()
+
+
+def click(window, button_text):
+    widgets = [window]
+    while True:
+        w = widgets.pop()
+        if isinstance(w, ttk.Button) and w["text"] == button_text:
+            w.invoke()
+            return
+        widgets.extend(w.winfo_children())
+
+
+def test_cancel(alice, mocker, monkeypatch, wait_until):
+    monkeypatch.setattr("tkinter.Toplevel.wait_window", lambda w: click(w, "Cancel"))
+    server_view = alice.get_server_views()[0]
+    server_view.show_config_dialog()
+
+    # Ensure nothing happened
+    alice.entry.insert("end", "lolwatwut")
+    alice.on_enter_pressed()
+    wait_until(lambda: "lolwatwut" in alice.text())
+    assert "Disconnected" not in alice.text()
+
+
+def test_reconnect(alice, mocker, monkeypatch, wait_until):
+    monkeypatch.setattr("tkinter.Toplevel.wait_window", lambda w: click(w, "Reconnect"))
+    server_view = alice.get_server_views()[0]
+    server_view.show_config_dialog()
+    wait_until(lambda: "Disconnected" in alice.text())
+
+
+def test_nothing_changes_if_you_only_click_reconnect(root_window, monkeypatch):
+    monkeypatch.setattr("tkinter.Toplevel.wait_window", lambda w: click(w, "Reconnect"))
+    sample_config = {
+        "host": "example.com",
+        "port": 1234,
+        "ssl": False,
+        "nick": "AzureDiamond",
+        "password": "hunter2",
+        "username": "azure69",
+        "realname": "xd lol",
+        "joined_channels": ["#lol", "#wut"],
+        "extra_notifications": ["#wut"],
+        "join_leave_hiding": {"show_by_default": True, "exception_nicks": []},
+    }
+    assert (
+        show_connection_settings_dialog(
+            transient_to=root_window, initial_config=sample_config
+        )
+        == sample_config
+    )
+
+
+def test_default_settings(root_window, monkeypatch):
+    monkeypatch.setattr("tkinter.Toplevel.wait_window", lambda w: click(w, "Connect!"))
+    config = show_connection_settings_dialog(
+        transient_to=root_window, initial_config=None
+    )
+    assert config.pop("nick") == config.pop("username") == config.pop("realname")
+    assert config == {
+        "host": "irc.libera.chat",
+        "joined_channels": ["##learnpython"],
+        "password": None,
+        "port": 6697,
+        "ssl": True,
+        "extra_notifications": [],
+        "join_leave_hiding": {"show_by_default": True, "exception_nicks": []},
+    }
+
+
+def test_join_part_quit_messages_disabled(alice, bob, wait_until, monkeypatch):
+    bob.entry.insert("end", "/join #lol")
+    bob.on_enter_pressed()
+    wait_until(lambda: "The topic of #lol is:" in bob.text())
+
+    # Configure Bob to ignore Alice joining/quitting
+    def bob_config(transient_to, initial_config):
+        new_config = copy.deepcopy(initial_config)
+        new_config["join_leave_hiding"]["exception_nicks"].append("aLiCe")
+        return new_config
+
+    monkeypatch.setattr("mantaray.config.show_connection_settings_dialog", bob_config)
+    bob.get_server_views()[0].show_config_dialog()
+
+    alice.entry.insert("end", "/join #lol")
+    alice.on_enter_pressed()
+    wait_until(lambda: "The topic of #lol is:" in alice.text())
+    alice.entry.insert("end", "/part #lol")
+    alice.on_enter_pressed()
+    wait_until(lambda: not alice.get_server_views()[0].find_channel("#lol"))
+    alice.entry.insert("end", "/join #lol")
+    alice.on_enter_pressed()
+    wait_until(lambda: "The topic of #lol is:" in alice.text())
+    alice.entry.insert("end", "Hello Bob")
+    alice.on_enter_pressed()
+    alice.entry.insert("end", "/quit")
+    alice.on_enter_pressed()
+    wait_until(lambda: not alice.winfo_exists())
+
+    wait_until(
+        lambda: "Hello Bob" in bob.text()
+        and "Alice" not in bob.get_current_view().userlist.get_nicks()
+    )
+    assert "joined" not in bob.text()
+    assert "left" not in bob.text()
+    assert "parted" not in bob.text()
+    assert "quit" not in bob.text()
--- tests/test_messaging.py
+++ tests/test_messaging.py
@@ -2,3 +2,155 @@ def test_basic(alice, bob, wait_until):
     alice.entry.insert(0, "Hello there")
     alice.on_enter_pressed()
     wait_until(lambda: "Hello there\n" in bob.text())
+
+
+def test_colors(alice, bob, wait_until):
+    alice.entry.insert(
+        "end",
+        "\x0311,4cyan on red\x0f \x02bold\x0f \x1funderline\x0f \x0311,4\x02\x1feverything\x0f nothing",
+    )
+    alice.on_enter_pressed()
+    wait_until(lambda: "cyan on red" in bob.text())
+
+    def tags(search_string):
+        index = bob.get_current_view().textwidget.search(search_string, "1.0")
+        return set(bob.get_current_view().textwidget.tag_names(index))
+
+    assert tags("cyan on red") == {"received-privmsg", "foreground-11", "background-4"}
+    assert tags("bold") == {"received-privmsg"}  # bolding not supported
+    assert tags("underline") == {"received-privmsg", "underline"}
+    assert tags("everything") == {
+        "received-privmsg",
+        "foreground-11",
+        "background-4",
+        "underline",
+    }
+    assert tags("nothing") == {"received-privmsg"}
+    assert "cyan on red bold underline everything nothing" in bob.text()
+
+
+def test_nick_autocompletion(alice, bob):
+    alice.entry.insert(0, "i think b")
+    alice.autocomplete()
+    # space at the end is important, so alice can easily finish the sentence
+    assert alice.entry.get() == "i think Bob "
+    assert alice.entry.index("insert") == len("i think Bob ")
+
+
+def test_nick_autocompletion_after_entering_message(alice, bob):
+    alice.entry.insert(0, "bhello there")
+    alice.entry.icursor(1)
+    alice.autocomplete()
+    assert alice.entry.get() == "Bob: hello there"
+    assert alice.entry.index("insert") == len("Bob: ")
+
+
+def test_escaped_slash(alice, bob, wait_until):
+    alice.entry.insert(0, "//home/alice/codes")
+    alice.on_enter_pressed()
+    wait_until(lambda: " /home/alice/codes\n" in bob.text())
+
+
+def test_enter_press_with_no_text(alice, bob, wait_until):
+    alice.on_enter_pressed()
+    assert "Alice" not in bob.text()
+
+
+def test_multiline_sending(alice, bob, wait_until, mocker):
+    mock = mocker.patch("tkinter.messagebox.askyesno")
+    mock.return_value = True
+    alice.entry.insert("end", "one\ntwo\nthree\nfour")
+    alice.on_enter_pressed()
+    assert not alice.entry.get()
+    mock.assert_called_once()
+
+    wait_until(lambda: "four" in bob.text())
+    i = bob.text().index
+    assert i("one") < i("two") < i("three") < i("four")
+
+
+def test_multiline_not_sending(alice, bob, wait_until, mocker):
+    mock = mocker.patch("tkinter.messagebox.askyesno")
+    mock.return_value = False
+    alice.entry.insert("end", "one\ntwo\nthree\nfour")
+    alice.on_enter_pressed()
+    mock.assert_called_once()
+    assert alice.entry.get() == "one\ntwo\nthree\nfour"
+
+
+def test_private_messages(alice, bob, wait_until):
+    # TODO: some button in gui to start private messaging?
+    # TODO: "/msg bob asdf" with lowercase bob causes two bugs:
+    #   - hircd doesn't send message
+    #   - this client thinks that Bob and bob are two different nicks
+
+    alice.entry.insert(0, "/msg Bob hello there")
+    alice.on_enter_pressed()
+    wait_until(lambda: "hello there" in alice.text())
+    wait_until(lambda: "hello there" in bob.text())
+    assert alice.get_current_view().view_name == "Bob"
+    assert bob.get_current_view().view_name == "Alice"
+
+    bob.entry.insert(0, "Hey Alice")
+    bob.on_enter_pressed()
+    wait_until(lambda: "Hey Alice" in alice.text())
+    wait_until(lambda: "Hey Alice" in bob.text())
+
+
+def test_history(alice, bob, wait_until):
+    # Alice presses first arrow up, then arrow down
+    assert not alice.entry.get()
+    alice.previous_message_to_entry()
+    assert not alice.entry.get()
+    alice.next_message_to_entry()
+    assert not alice.entry.get()
+
+    # Bob presses first arrow down, then arrow up
+    assert not alice.entry.get()
+    alice.next_message_to_entry()
+    assert not alice.entry.get()
+    alice.previous_message_to_entry()
+    assert not alice.entry.get()
+
+    alice.entry.insert(0, "first message")
+    alice.on_enter_pressed()
+    wait_until(lambda: "first message" in alice.text())
+
+    assert not alice.entry.get()
+    alice.previous_message_to_entry()
+    assert alice.entry.get() == "first message"
+    alice.previous_message_to_entry()
+    assert alice.entry.get() == "first message"
+    alice.next_message_to_entry()
+    assert alice.entry.get() == "first message"
+    alice.next_message_to_entry()
+
+    alice.entry.delete(0, "end")
+    alice.entry.insert(0, "second message")
+    alice.on_enter_pressed()
+    wait_until(lambda: "second message" in alice.text())
+
+    assert not alice.entry.get()
+    alice.previous_message_to_entry()
+    assert alice.entry.get() == "second message"
+    alice.previous_message_to_entry()
+    assert alice.entry.get() == "first message"
+    alice.previous_message_to_entry()
+    assert alice.entry.get() == "first message"
+    alice.next_message_to_entry()
+    assert alice.entry.get() == "second message"
+    alice.next_message_to_entry()
+    assert alice.entry.get() == "second message"
+
+    alice.entry.delete(0, "end")
+    alice.entry.insert(0, "//escaped message")
+    alice.on_enter_pressed()
+    wait_until(lambda: "escaped message" in alice.text())
+
+    assert not alice.entry.get()
+    alice.previous_message_to_entry()
+    assert alice.entry.get() == "//escaped message"
+    alice.previous_message_to_entry()
+    assert alice.entry.get() == "second message"
+    alice.next_message_to_entry()
+    assert alice.entry.get() == "//escaped message"
