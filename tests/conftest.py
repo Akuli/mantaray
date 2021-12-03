96d061e83917d4fef4a9f8f0dc51505caa151530
Akuli <akuviljanen17@gmail.com>
2021-12-03 11:47:28 +0200

    switch up order

--- tests/conftest.py
+++ tests/conftest.py
@@ -120,11 +120,11 @@ def alice_and_bob(hircd, root_window, wait_until, mocker):
     yield widgets
 
     for irc_widget in widgets.values():
-        shutil.rmtree(irc_widget.log_dir)
         if irc_widget.winfo_exists():
             for server_view in irc_widget.get_server_views():
                 server_view.core.quit()
                 server_view.core.wait_until_stopped()
+        shutil.rmtree(irc_widget.log_dir)
 
 
 @pytest.fixture
