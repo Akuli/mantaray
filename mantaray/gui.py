9015e8343befdc1ef852ebfe6b7bb97f57150b04
Akuli <akuviljanen17@gmail.com>
2021-12-27 21:58:37 +0200

    reduce gui.py

--- mantaray/gui.py
+++ mantaray/gui.py
@@ -31,14 +31,6 @@ class IrcWidget(ttk.PanedWindow):
         self._previous_view: View | None = None
         self.view_selector.bind("<<TreeviewSelect>>", self._current_view_changed)
 
-        if sys.platform == "darwin":
-            self.view_selector.bind("<Button-2>", self._view_selector_right_click)
-            self.view_selector.bind(
-                "<Control-Button-1>", self._view_selector_right_click
-            )
-        else:
-            self.view_selector.bind("<Button-3>", self._view_selector_right_click)
-
         self._middle_pane = ttk.Frame(self)
         self.add(self._middle_pane, weight=1)  # always stretch
 
@@ -53,8 +45,6 @@ class IrcWidget(ttk.PanedWindow):
             insertbackground=colors.FOREGROUND,
         )
         self.entry.pack(side="left", fill="both", expand=True)
-        self.entry.bind("<Prior>", self._scroll_up)
-        self.entry.bind("<Next>", self._scroll_down)
 
         # {channel_like.name: channel_like}
         self.views_by_id: dict[str, View] = {}
@@ -65,10 +55,6 @@ class IrcWidget(ttk.PanedWindow):
         [view_id] = self.view_selector.selection()
         return self.views_by_id[view_id]
 
-    # for tests
-    def text(self) -> str:
-        return self.get_current_view().textwidget.get("1.0", "end - 1 char")
-
     def get_server_views(self) -> list[ServerView]:
         result = []
         for view_id in self.view_selector.get_children(""):
@@ -76,70 +62,6 @@ class IrcWidget(ttk.PanedWindow):
             assert isinstance(view, ServerView)
             result.append(view)
         return result
-
-    def _scroll_up(self, junk_event: object) -> None:
-        self.get_current_view().textwidget.yview_scroll(-1, "pages")
-
-    def _scroll_down(self, junk_event: object) -> None:
-        self.get_current_view().textwidget.yview_scroll(1, "pages")
-
-    def bigger_font_size(self) -> None:
-        self.font["size"] += 1
-
-    def smaller_font_size(self) -> None:
-        if self.font["size"] > 3:
-            self.font["size"] -= 1
-
-    def _get_flat_list_of_item_ids(self) -> list[str]:
-        result = []
-        for server_id in self.view_selector.get_children(""):
-            result.append(server_id)
-            result.extend(self.view_selector.get_children(server_id))
-        return result
-
-    def select_by_number(self, index: int) -> None:
-        ids = self._get_flat_list_of_item_ids()
-        try:
-            self.view_selector.selection_set(ids[index])
-        except IndexError:
-            pass
-
-    def select_previous_view(self) -> None:
-        ids = self._get_flat_list_of_item_ids()
-        index = ids.index(self.get_current_view().view_id) - 1
-        if index >= 0:
-            self.view_selector.selection_set(ids[index])
-
-    def select_next_view(self) -> None:
-        ids = self._get_flat_list_of_item_ids()
-        index = ids.index(self.get_current_view().view_id) + 1
-        if index < len(ids):
-            self.view_selector.selection_set(ids[index])
-
-    def _select_another_view(self, bad_view: View) -> None:
-        if self.get_current_view() == bad_view:
-            ids = self._get_flat_list_of_item_ids()
-            index = ids.index(bad_view.view_id)
-            if index == 0:
-                self.view_selector.selection_set(ids[1])
-            else:
-                self.view_selector.selection_set(ids[index - 1])
-
-    def move_view_up(self) -> None:
-        view_id = self.get_current_view().view_id
-        self.view_selector.move(
-            view_id,
-            self.view_selector.parent(view_id),
-            self.view_selector.index(view_id) - 1,
-        )
-
-    def move_view_down(self) -> None:
-        view_id = self.get_current_view().view_id
-        self.view_selector.move(
-            view_id,
-            self.view_selector.parent(view_id),
-            self.view_selector.index(view_id) + 1,
-        )
 
     def _current_view_changed(self, event: object) -> None:
         new_view = self.get_current_view()
@@ -163,64 +85,3 @@ class IrcWidget(ttk.PanedWindow):
         self.view_selector.item(view.server_view.view_id, open=True)
         self.views_by_id[view.view_id] = view
         self.view_selector.selection_set(view.view_id)
-
-    def remove_view(self, view: ChannelView | PMView) -> None:
-        self._select_another_view(view)
-        self.view_selector.delete(view.view_id)
-        view.close_log_file()
-        view.destroy_widgets()
-        del self.views_by_id[view.view_id]
-
-    def remove_server(self, server_view: ServerView) -> None:
-        for subview in server_view.get_subviews():
-            assert isinstance(subview, (ChannelView, PMView))
-            self.remove_view(subview)
-
-        if len(self.view_selector.get_children("")) == 1:
-            self.destroy()
-        else:
-            self._select_another_view(server_view)
-            self.view_selector.delete(server_view.view_id)
-            server_view.close_log_file()
-            server_view.destroy_widgets()
-            del self.views_by_id[server_view.view_id]
-
-    def _fill_menu(self) -> None:
-        view = self.get_current_view()
-
-        if isinstance(view, ChannelView):
-
-            def on_change(*junk: object) -> None:
-                assert isinstance(view, ChannelView)  # mypy awesomeness
-                view.server_view.extra_notifications ^= {view.channel_name}
-
-            var = tkinter.BooleanVar(
-                value=(view.channel_name in view.server_view.extra_notifications)
-            )
-            var.trace_add("write", on_change)
-            self._garbage_collection_is_lol = var
-            self._contextmenu.add_checkbutton(
-                label="Show notifications for all messages", variable=var
-            )
-
-    def _view_selector_right_click(
-        self, event: tkinter.Event[tkinter.ttk.Treeview]
-    ) -> None:
-        item_id = self.view_selector.identify_row(event.y)
-        if not item_id:
-            return
-        self.view_selector.selection_set(item_id)
-
-        self._contextmenu.delete(0, "end")
-        self._fill_menu()
-        self._contextmenu.tk_popup(event.x_root + 5, event.y_root)
-
-    def get_current_config(self) -> config.Config:
-        return {
-            "servers": [
-                server_view.get_current_config()
-                for server_view in self.get_server_views()
-            ],
-            "font_family": self.font["family"],
-            "font_size": self.font["size"],
-        }
