5c4305f280be670c3b1832675793a878c1c7db79
Akuli <akuviljanen17@gmail.com>
2021-12-27 18:24:41 +0200

    add back test_part_last_channel

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,5 +1,7 @@
 import pytest
 
+from mantaray.views import ServerView
+
 
 @pytest.mark.parametrize("part_command", ["/part", "/part #lol"])
 def test_join_and_part(alice, bob, wait_until, part_command):
@@ -27,3 +29,9 @@ def test_join_and_part(alice, bob, wait_until, part_command):
     wait_until(lambda: not bob.get_server_views()[0].find_channel("#lol"))
     wait_until(lambda: "Bob left #lol.\n" in alice.text())
     assert bob.get_current_config()["servers"][0]["joined_channels"] == ["#autojoin"]
+
+
+def test_part_last_channel(alice, bob, wait_until):
+    alice.entry.insert("end", "/part #autojoin")
+    alice.on_enter_pressed()
+    wait_until(lambda: isinstance(alice.get_current_view(), ServerView))
