f9389428a93e5243c9f36c7d1573775ba0022551
Akuli <akuviljanen17@gmail.com>
2021-12-28 00:18:07 +0200

    IrcWidget goes global

--- a.py
+++ a.py
@@ -17,14 +17,14 @@ def ServerView__init__(irc_widget):
     global ServerView_extra_notifications
     global ServerView__join_leave_hiding_config
     ServerView_irc_widget = irc_widget
-    ServerView_view_id = irc_widget.view_selector.insert("", "end", text="localhost")
+    ServerView_view_id = view_selector.insert("", "end", text="localhost")
     ServerView_notification_count = 0
 
     ServerView_textwidget = tkinter.Text(
         irc_widget,
         width=1,
         height=1,
-        font=irc_widget.font,
+        font=font,
         state="disabled",
         takefocus=True,
     )
@@ -56,7 +56,7 @@ def ServerView_handle_events() -> None:
         except queue.Empty:
             break
 
-        assert not ServerView_irc_widget.view_selector.get_children(ServerView_view_id)
+        assert not view_selector.get_children(ServerView_view_id)
         sender = ""
         chunks = ((message, ["info"]),)
         do_the_scroll = ServerView_textwidget.yview()[1] == 1.0
@@ -83,65 +83,61 @@ def ServerView_handle_events() -> None:
             ServerView_textwidget.see("end")
 
 
-class IrcWidget(ttk.PanedWindow):
-    def __init__(self, master: tkinter.Misc):
-        super().__init__(master, orient="horizontal")
-        self.font = Font()
-
-        self.view_selector = ttk.Treeview(self, show="tree", selectmode="browse")
-        self.view_selector.tag_configure("pinged", foreground="#00ff00")
-        self.view_selector.tag_configure("new_message", foreground="#ffcc66")
-        self.add(self.view_selector, weight=0)
-        self._contextmenu = tkinter.Menu(tearoff=False)
-
-        self._previous_view_id = None
-        self.view_selector.bind("<<TreeviewSelect>>", self._current_view_changed)
-
-        self._middle_pane = ttk.Frame(self)
-        self.add(self._middle_pane, weight=1)
-
-        entryframe = ttk.Frame(self._middle_pane)
-        entryframe.pack(side="bottom", fill="x")
-
-        self.entry = tkinter.Entry(
-            entryframe,
-            font=self.font,
-        )
-        self.entry.pack(side="left", fill="both", expand=True)
-
-        ServerView__init__(self)
-        self.view_selector.item(ServerView_view_id, open=True)
-        self.view_selector.selection_set(ServerView_view_id)
-
-    def _current_view_changed(self, event: object) -> None:
-        view_id = ServerView_view_id
-        if self._previous_view_id == view_id:
-            return
-
-        if (
-            self._previous_view_id is not None
-            and ServerView_textwidget.winfo_exists()
-        ):
-            ServerView_textwidget.pack_forget()
-        ServerView_textwidget.pack(
-            in_=self._middle_pane, side="top", fill="both", expand=True
-        )
-        global ServerView_notification_count
-        ServerView_notification_count = 0
-        ServerView_irc_widget.event_generate("<<NotificationCountChanged>>")
-
-        old_tags = set(ServerView_irc_widget.view_selector.item(ServerView_view_id, "tags"))
-        ServerView_irc_widget.view_selector.item(
-            ServerView_view_id, tags=list(old_tags - {"new_message", "pinged"})
-        )
-
-        self._previous_view_id = view_id
-
-
 root_window = tkinter.Tk()
-alice = IrcWidget(
-    root_window,
+alice = ttk.PanedWindow(root_window, orient="horizontal")
+
+def _current_view_changed(event: object) -> None:
+    global _previous_view_id
+    view_id = ServerView_view_id
+    if _previous_view_id == view_id:
+        return
+
+    if (
+        _previous_view_id is not None
+        and ServerView_textwidget.winfo_exists()
+    ):
+        ServerView_textwidget.pack_forget()
+    ServerView_textwidget.pack(
+        in_=_middle_pane, side="top", fill="both", expand=True
+    )
+    global ServerView_notification_count
+    ServerView_notification_count = 0
+    ServerView_irc_widget.event_generate("<<NotificationCountChanged>>")
+
+    old_tags = set(view_selector.item(ServerView_view_id, "tags"))
+    view_selector.item(
+        ServerView_view_id, tags=list(old_tags - {"new_message", "pinged"})
+    )
+
+    _previous_view_id = view_id
+
+font = Font()
+
+view_selector = ttk.Treeview(alice, show="tree", selectmode="browse")
+view_selector.tag_configure("pinged", foreground="#00ff00")
+view_selector.tag_configure("new_message", foreground="#ffcc66")
+alice.add(view_selector, weight=0)
+_contextmenu = tkinter.Menu(tearoff=False)
+
+_previous_view_id = None
+view_selector.bind("<<TreeviewSelect>>", _current_view_changed)
+
+_middle_pane = ttk.Frame(alice)
+alice.add(_middle_pane, weight=1)
+
+entryframe = ttk.Frame(_middle_pane)
+entryframe.pack(side="bottom", fill="x")
+
+entry = tkinter.Entry(
+    entryframe,
+    font=font,
 )
+entry.pack(side="left", fill="both", expand=True)
+
+ServerView__init__(alice)
+view_selector.item(ServerView_view_id, open=True)
+view_selector.selection_set(ServerView_view_id)
+
 alice.pack(fill="both", expand=True)
 
 end = time.monotonic() + 5
