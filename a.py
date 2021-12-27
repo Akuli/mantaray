ef440c9eb409d6bfde8d94ec365e1fd893f57905
Akuli <akuviljanen17@gmail.com>
2021-12-27 21:28:19 +0200

    simplify

--- a.py
+++ a.py
@@ -4,48 +4,41 @@ import subprocess
 import tkinter
 import tempfile
 from pathlib import Path
-import traceback
 
 from mantaray import gui, config
 
 
 print(50*"A", flush=True)
 root_window = tkinter.Tk()
-try:
-    print(50*"C", flush=True)
-    clone_url = "https://github.com/fboender/hircd"
-    hircd_repo = Path(__file__).absolute().parent / "hircd"
-    subprocess.check_call(["git", "clone", clone_url], cwd=hircd_repo.parent)
-    process = subprocess.Popen(
-        [sys.executable, "hircd.py", "--foreground", "--verbose", "--log-stdout"],
-        stderr=subprocess.PIPE,
-        cwd=hircd_repo,
-    )
-    for line in process.stderr:
-        assert b"ERROR" not in line
-        if line.startswith(b"[INFO] Starting hircd on "):
-            break
+print(50*"C", flush=True)
+clone_url = "https://github.com/fboender/hircd"
+hircd_repo = Path(__file__).absolute().parent / "hircd"
+subprocess.check_call(["git", "clone", clone_url], cwd=hircd_repo.parent)
+process = subprocess.Popen(
+    [sys.executable, "hircd.py", "--foreground", "--verbose", "--log-stdout"],
+    stderr=subprocess.PIPE,
+    cwd=hircd_repo,
+)
+for line in process.stderr:
+    assert b"ERROR" not in line
+    if line.startswith(b"[INFO] Starting hircd on "):
+        break
 
-    print(50*"D", flush=True)
-    alice = gui.IrcWidget(
-        root_window,
-        config.load_from_file(Path("alice")),
-        Path(tempfile.mkdtemp(prefix="mantaray-tests-")),
-    )
-    print(50*"E", flush=True)
-    alice.pack(fill="both", expand=True)
-    print(50*"F", flush=True)
+print(50*"D", flush=True)
+alice = gui.IrcWidget(
+    root_window,
+    config.load_from_file(Path("alice")),
+    Path(tempfile.mkdtemp(prefix="mantaray-tests-")),
+)
+print(50*"E", flush=True)
+alice.pack(fill="both", expand=True)
+print(50*"F", flush=True)
 
-    end = time.monotonic() + 5
-    while time.monotonic() < end:
-        root_window.update()
-        if "The topic of #autojoin is" in alice.text():
-            break
-    else:
-        raise RuntimeError("timed out waiting")
+end = time.monotonic() + 5
+while time.monotonic() < end:
+    root_window.update()
+    alice.text()
 
-finally:
-    traceback.print_exc()
-    print(50*"W", flush=True)
-    root_window.destroy()
-    print(50*"X", flush=True)
+print(50*"W", flush=True)
+root_window.destroy()
+print(50*"X", flush=True)
