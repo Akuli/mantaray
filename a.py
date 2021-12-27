dea95661778cfb446649894622c813d0a3b18155
Akuli <akuviljanen17@gmail.com>
2021-12-28 00:07:31 +0200

    ServerView goes global

--- a.py
+++ a.py
@@ -8,72 +8,79 @@ from tkinter import ttk
 from tkinter.font import Font
 
 
-class ServerView:
-    def __init__(self, irc_widget):
-        self.irc_widget = irc_widget
-        self.view_id = irc_widget.view_selector.insert("", "end", text="localhost")
-        self.notification_count = 0
+def ServerView__init__(irc_widget):
+    global ServerView_irc_widget
+    global ServerView_view_id
+    global ServerView_notification_count
+    global ServerView_textwidget
+    global ServerView_event_queue
+    global ServerView_extra_notifications
+    global ServerView__join_leave_hiding_config
+    ServerView_irc_widget = irc_widget
+    ServerView_view_id = irc_widget.view_selector.insert("", "end", text="localhost")
+    ServerView_notification_count = 0
 
-        self.textwidget = tkinter.Text(
-            irc_widget,
-            width=1,
-            height=1,
-            font=irc_widget.font,
-            state="disabled",
-            takefocus=True,
-        )
-        self.textwidget.bind("<Button-1>", (lambda e: self.textwidget.focus()))
+    ServerView_textwidget = tkinter.Text(
+        irc_widget,
+        width=1,
+        height=1,
+        font=irc_widget.font,
+        state="disabled",
+        takefocus=True,
+    )
+    ServerView_textwidget.bind("<Button-1>", (lambda e: ServerView_textwidget.focus()))
 
-        self.textwidget.tag_configure("underline", underline=True)
-        self.textwidget.tag_configure("pinged", foreground="black")
-        self.textwidget.tag_configure("error", foreground="black")
-        self.textwidget.tag_configure("info", foreground="red")
-        self.textwidget.tag_configure("history-selection", background="red")
-        self.textwidget.tag_configure("channel", foreground="red")
-        self.textwidget.tag_configure("self-nick", foreground="red", underline=True)
-        self.textwidget.tag_configure("other-nick", foreground="red", underline=True)
+    ServerView_textwidget.tag_configure("underline", underline=True)
+    ServerView_textwidget.tag_configure("pinged", foreground="black")
+    ServerView_textwidget.tag_configure("error", foreground="black")
+    ServerView_textwidget.tag_configure("info", foreground="red")
+    ServerView_textwidget.tag_configure("history-selection", background="red")
+    ServerView_textwidget.tag_configure("channel", foreground="red")
+    ServerView_textwidget.tag_configure("self-nick", foreground="red", underline=True)
+    ServerView_textwidget.tag_configure("other-nick", foreground="red", underline=True)
 
-        self.event_queue = queue.Queue()
-        self.extra_notifications = set()
-        self._join_leave_hiding_config = {"show_by_default": True, "exception_nicks": []}
+    ServerView_event_queue = queue.Queue()
+    ServerView_extra_notifications = set()
+    ServerView__join_leave_hiding_config = {"show_by_default": True, "exception_nicks": []}
 
-        self.event_queue.put("Blah...")
-        self.handle_events()
+    ServerView_event_queue.put("Blah...")
+    ServerView_handle_events()
 
-    def handle_events(self) -> None:
-        self.irc_widget.after(100, self.handle_events)
 
-        while True:
-            try:
-                message = self.event_queue.get(block=False)
-            except queue.Empty:
-                break
+def ServerView_handle_events() -> None:
+    ServerView_irc_widget.after(100, ServerView_handle_events)
 
-            assert not self.irc_widget.view_selector.get_children(self.view_id)
-            sender = ""
-            chunks = ((message, ["info"]),)
-            do_the_scroll = self.textwidget.yview()[1] == 1.0
-            padding = " " * (16 - len(sender))
+    while True:
+        try:
+            message = ServerView_event_queue.get(block=False)
+        except queue.Empty:
+            break
 
-            if sender == "*":
-                sender_tags = []
-            elif sender == "Alice":
-                sender_tags = ["self-nick"]
-            else:
-                sender_tags = ["other-nick"]
+        assert not ServerView_irc_widget.view_selector.get_children(ServerView_view_id)
+        sender = ""
+        chunks = ((message, ["info"]),)
+        do_the_scroll = ServerView_textwidget.yview()[1] == 1.0
+        padding = " " * (16 - len(sender))
 
-            self.textwidget.config(state="normal")
-            self.textwidget.insert("end", time.strftime("[%H:%M]") + " " + padding)
-            self.textwidget.insert("end", sender, sender_tags)
-            self.textwidget.insert("end", " | ")
-            flatten = itertools.chain.from_iterable
-            if chunks:
-                self.textwidget.insert("end", *flatten(chunks))
-            self.textwidget.insert("end", "\n")
-            self.textwidget.config(state="disabled")
+        if sender == "*":
+            sender_tags = []
+        elif sender == "Alice":
+            sender_tags = ["self-nick"]
+        else:
+            sender_tags = ["other-nick"]
 
-            if do_the_scroll:
-                self.textwidget.see("end")
+        ServerView_textwidget.config(state="normal")
+        ServerView_textwidget.insert("end", time.strftime("[%H:%M]") + " " + padding)
+        ServerView_textwidget.insert("end", sender, sender_tags)
+        ServerView_textwidget.insert("end", " | ")
+        flatten = itertools.chain.from_iterable
+        if chunks:
+            ServerView_textwidget.insert("end", *flatten(chunks))
+        ServerView_textwidget.insert("end", "\n")
+        ServerView_textwidget.config(state="disabled")
+
+        if do_the_scroll:
+            ServerView_textwidget.see("end")
 
 
 class IrcWidget(ttk.PanedWindow):
@@ -87,7 +94,7 @@ class IrcWidget(ttk.PanedWindow):
         self.add(self.view_selector, weight=0)
         self._contextmenu = tkinter.Menu(tearoff=False)
 
-        self._previous_view = None
+        self._previous_view_id = None
         self.view_selector.bind("<<TreeviewSelect>>", self._current_view_changed)
 
         self._middle_pane = ttk.Frame(self)
@@ -102,34 +109,33 @@ class IrcWidget(ttk.PanedWindow):
         )
         self.entry.pack(side="left", fill="both", expand=True)
 
-        view = ServerView(self)
-        self.views_by_id = {view.view_id: view}
-        self.view_selector.item(view.view_id, open=True)
-        self.view_selector.selection_set(view.view_id)
+        ServerView__init__(self)
+        self.view_selector.item(ServerView_view_id, open=True)
+        self.view_selector.selection_set(ServerView_view_id)
 
     def _current_view_changed(self, event: object) -> None:
-        [view_id] = self.view_selector.selection()
-        new_view = self.views_by_id[view_id]
-        if self._previous_view == new_view:
+        view_id = ServerView_view_id
+        if self._previous_view_id == view_id:
             return
 
         if (
-            self._previous_view is not None
-            and self._previous_view.textwidget.winfo_exists()
+            self._previous_view_id is not None
+            and ServerView_textwidget.winfo_exists()
         ):
-            self._previous_view.textwidget.pack_forget()
-        new_view.textwidget.pack(
+            ServerView_textwidget.pack_forget()
+        ServerView_textwidget.pack(
             in_=self._middle_pane, side="top", fill="both", expand=True
         )
-        new_view.notification_count = 0
-        new_view.irc_widget.event_generate("<<NotificationCountChanged>>")
+        global ServerView_notification_count
+        ServerView_notification_count = 0
+        ServerView_irc_widget.event_generate("<<NotificationCountChanged>>")
 
-        old_tags = set(new_view.irc_widget.view_selector.item(new_view.view_id, "tags"))
-        new_view.irc_widget.view_selector.item(
-            new_view.view_id, tags=list(old_tags - {"new_message", "pinged"})
+        old_tags = set(ServerView_irc_widget.view_selector.item(ServerView_view_id, "tags"))
+        ServerView_irc_widget.view_selector.item(
+            ServerView_view_id, tags=list(old_tags - {"new_message", "pinged"})
         )
 
-        self._previous_view = new_view
+        self._previous_view_id = view_id
 
 
 root_window = tkinter.Tk()
