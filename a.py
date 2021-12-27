43a073fbfa84d84ae8ca1b7d9e40c2567b3f5c6d
Akuli <akuviljanen17@gmail.com>
2021-12-28 01:09:40 +0200

    swaps

--- a.py
+++ a.py
@@ -7,15 +7,15 @@ from tkinter import ttk
 
 root_window = tkinter.Tk()
 panedwindow = ttk.PanedWindow(root_window)
+panedwindow.pack()
 
 treeview = ttk.Treeview(panedwindow)
 treeview.insert("", "end")
 panedwindow.add(treeview)
 
 middle_pane = ttk.Frame(panedwindow)
-panedwindow.add(middle_pane)
 tkinter.Entry(middle_pane).pack()
-panedwindow.pack()
+panedwindow.add(middle_pane)
 
 tkinter.Text(panedwindow).pack(in_=middle_pane)
 
