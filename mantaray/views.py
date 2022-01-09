3c6e6001b05ce891978f9b7bf03ba0cd3b606edd
Akuli <akuviljanen17@gmail.com>
2022-01-09 13:37:06 +0200

    black

--- mantaray/views.py
+++ mantaray/views.py
@@ -47,7 +47,9 @@ def _show_popup(title: str, text: str) -> None:
         elif sys.platform == "darwin":
             # https://stackoverflow.com/a/41318195
             command = (
-                "on run argv\n" "  display notification (item 2 of argv) with title (item 1 of argv)\n" "end run\n"
+                "on run argv\n"
+                "  display notification (item 2 of argv) with title (item 1 of argv)\n"
+                "end run\n"
             )
             subprocess.call(["osascript", "-e", command, title, text])
         else:
@@ -57,7 +59,12 @@ def _show_popup(title: str, text: str) -> None:
 
 
 def _parse_privmsg(
-    sender: str, message: str, self_nick: str, all_nicks: Sequence[str], *, pinged: bool = False
+    sender: str,
+    message: str,
+    self_nick: str,
+    all_nicks: Sequence[str],
+    *,
+    pinged: bool = False,
 ) -> tuple[str, list[tuple[str, list[str]]]]:
     sent = sender.lower() == self_nick.lower()
     chunks = []
@@ -72,7 +79,9 @@ def _parse_privmsg(
         sender = "*"
 
     for substring, base_tags in colors.parse_text(message):
-        for subsubstring, nick_tag in backend.find_nicks(substring, self_nick, all_nicks):
+        for subsubstring, nick_tag in backend.find_nicks(
+            substring, self_nick, all_nicks
+        ):
             tags = base_tags.copy()
             if nick_tag is not None:
                 tags.append(nick_tag)
@@ -117,7 +126,9 @@ class View:
 
     def reopen_log_file(self) -> None:
         self.close_log_file()
-        self.log_file = self.irc_widget.log_manager.open_log_file(self.server_view.view_name, self.get_log_name())
+        self.log_file = self.irc_widget.log_manager.open_log_file(
+            self.server_view.view_name, self.get_log_name()
+        )
 
     def _update_view_selector(self) -> None:
         if self.notification_count == 0:
@@ -156,7 +167,9 @@ class View:
         self.irc_widget.event_generate("<<NotificationCountChanged>>")
 
         old_tags = set(self.irc_widget.view_selector.item(self.view_id, "tags"))
-        self.irc_widget.view_selector.item(self.view_id, tags=list(old_tags - {"new_message", "pinged"}))
+        self.irc_widget.view_selector.item(
+            self.view_id, tags=list(old_tags - {"new_message", "pinged"})
+        )
 
     def add_tag(self, tag: Literal["new_message", "pinged"]) -> None:
         if self.irc_widget.get_current_view() == self:
@@ -166,7 +179,9 @@ class View:
         if "pinged" in old_tags:  # Adding tag does not unping
             return
 
-        self.irc_widget.view_selector.item(self.view_id, tags=list((old_tags - {"new_message", "pinged"}) | {tag}))
+        self.irc_widget.view_selector.item(
+            self.view_id, tags=list((old_tags - {"new_message", "pinged"}) | {tag})
+        )
 
     def destroy_widgets(self) -> None:
         self.textwidget.destroy()
@@ -179,7 +194,11 @@ class View:
         return parent_view
 
     def add_message(
-        self, sender: str, *chunks: tuple[str, list[str]], pinged: bool = False, show_in_gui: bool = True
+        self,
+        sender: str,
+        *chunks: tuple[str, list[str]],
+        pinged: bool = False,
+        show_in_gui: bool = True,
     ) -> None:
         if show_in_gui:
             # scroll down all the way if the user hasn't scrolled up manually
@@ -216,7 +235,12 @@ class View:
 
         if self.log_file is not None:
             print(
-                time.asctime(), sender, "".join(text for text, tags in chunks), sep="\t", file=self.log_file, flush=True
+                time.asctime(),
+                sender,
+                "".join(text for text, tags in chunks),
+                sep="\t",
+                file=self.log_file,
+                flush=True,
             )
 
     def on_connectivity_message(self, message: str, *, error: bool = False) -> None:
@@ -224,13 +248,21 @@ class View:
 
     def on_self_changed_nick(self, old: str, new: str) -> None:
         # notify about the nick change everywhere, by putting this to base class
-        self.add_message("*", ("You are now known as ", []), (new, ["self-nick"]), (".", []))
+        self.add_message(
+            "*", ("You are now known as ", []), (new, ["self-nick"]), (".", [])
+        )
 
     def get_relevant_nicks(self) -> Sequence[str]:
         return []
 
     def on_relevant_user_changed_nick(self, old: str, new: str) -> None:
-        self.add_message("*", (old, ["other-nick"]), (" is now known as ", []), (new, ["other-nick"]), (".", []))
+        self.add_message(
+            "*",
+            (old, ["other-nick"]),
+            (" is now known as ", []),
+            (new, ["other-nick"]),
+            (".", []),
+        )
 
     def on_relevant_user_quit(self, nick: str, reason: str | None) -> None:
         if reason is None:
@@ -248,7 +280,9 @@ class View:
 class ServerView(View):
     core: backend.IrcCore  # no idea why mypy need this
 
-    def __init__(self, irc_widget: IrcWidget, server_config: config.ServerConfig, verbose: bool):
+    def __init__(
+        self, irc_widget: IrcWidget, server_config: config.ServerConfig, verbose: bool
+    ):
         super().__init__(irc_widget, server_config["host"])
         self.core = backend.IrcCore(server_config, verbose=verbose)
         self.extra_notifications = set(server_config["extra_notifications"])
@@ -270,7 +304,9 @@ class ServerView(View):
         return self
 
     def should_show_join_leave_message(self, nick: str) -> bool:
-        is_exceptional = nick.lower() in (n.lower() for n in self._join_leave_hiding_config["exception_nicks"])
+        is_exceptional = nick.lower() in (
+            n.lower() for n in self._join_leave_hiding_config["exception_nicks"]
+        )
         return self._join_leave_hiding_config["show_by_default"] ^ is_exceptional
 
     def get_subviews(self, *, include_server: bool = False) -> list[View]:
@@ -283,7 +319,10 @@ class ServerView(View):
 
     def find_channel(self, name: str) -> ChannelView | None:
         for view in self.get_subviews():
-            if isinstance(view, ChannelView) and view.channel_name.lower() == name.lower():
+            if (
+                isinstance(view, ChannelView)
+                and view.channel_name.lower() == name.lower()
+            ):
                 return view
         return None
 
@@ -350,7 +389,9 @@ class ServerView(View):
             elif isinstance(event, backend.Kick):
                 channel_view = self.find_channel(event.channel)
                 assert channel_view is not None
-                channel_view.on_kick(event.channel, event.kicker, event.kicked_nick, event.reason)
+                channel_view.on_kick(
+                    event.channel, event.kicker, event.kicked_nick, event.reason
+                )
 
             elif isinstance(event, backend.UserQuit):
                 for view in self.get_subviews(include_server=True):
@@ -365,7 +406,9 @@ class ServerView(View):
             elif isinstance(event, backend.SentPrivmsg):
                 channel_view = self.find_channel(event.recipient)
                 if channel_view is None:
-                    assert not re.fullmatch(backend.CHANNEL_REGEX, event.recipient), event.recipient
+                    assert not re.fullmatch(
+                        backend.CHANNEL_REGEX, event.recipient
+                    ), event.recipient
                     pm_view = self.find_pm(event.recipient)
                     if pm_view is None:
                         # start of a new PM conversation
@@ -393,15 +436,21 @@ class ServerView(View):
 
                     pinged = any(
                         tag == "self-nick"
-                        for substring, tag in backend.find_nicks(event.text, self.core.nick, [self.core.nick])
+                        for substring, tag in backend.find_nicks(
+                            event.text, self.core.nick, [self.core.nick]
+                        )
                     )
                     channel_view.on_privmsg(event.sender, event.text, pinged=pinged)
                     channel_view.add_tag("pinged" if pinged else "new_message")
-                    if pinged or (channel_view.channel_name in self.extra_notifications):
+                    if pinged or (
+                        channel_view.channel_name in self.extra_notifications
+                    ):
                         channel_view.add_notification(f"<{event.sender}> {event.text}")
 
             elif isinstance(event, (backend.ServerMessage, backend.UnknownMessage)):
-                self.server_view.add_message(event.sender or "???", (" ".join([event.command] + event.args), []))
+                self.server_view.add_message(
+                    event.sender or "???", (" ".join([event.command] + event.args), [])
+                )
 
             elif isinstance(event, backend.ConnectivityMessage):
                 for view in self.get_subviews(include_server=True):
@@ -423,7 +472,11 @@ class ServerView(View):
                 print("can't happen")  # type: ignore
 
     def get_current_config(self) -> config.ServerConfig:
-        channels = [view.channel_name for view in self.get_subviews() if isinstance(view, ChannelView)]
+        channels = [
+            view.channel_name
+            for view in self.get_subviews()
+            if isinstance(view, ChannelView)
+        ]
         return {
             "host": self.core.host,
             "port": self.core.port,
@@ -433,7 +486,8 @@ class ServerView(View):
             "realname": self.core.realname,
             "password": self.core.password,
             "joined_channels": sorted(
-                self.core.autojoin, key=(lambda chan: channels.index(chan) if chan in channels else -1)
+                self.core.autojoin,
+                key=(lambda chan: channels.index(chan) if chan in channels else -1),
             ),
             "extra_notifications": list(self.extra_notifications),
             "join_leave_hiding": self._join_leave_hiding_config,
@@ -442,7 +496,8 @@ class ServerView(View):
 
     def show_config_dialog(self) -> None:
         new_config = config.show_connection_settings_dialog(
-            transient_to=self.irc_widget.winfo_toplevel(), initial_config=self.get_current_config()
+            transient_to=self.irc_widget.winfo_toplevel(),
+            initial_config=self.get_current_config(),
         )
         if new_config is not None:
             self._join_leave_hiding_config = new_config["join_leave_hiding"]
@@ -450,7 +505,10 @@ class ServerView(View):
             self.audio_notification = new_config["audio_notification"]
             # TODO: autojoin setting would be better in right-click
             for subview in self.get_subviews():
-                if isinstance(subview, ChannelView) and subview.channel_name not in self.core.autojoin:
+                if (
+                    isinstance(subview, ChannelView)
+                    and subview.channel_name not in self.core.autojoin
+                ):
                     self.irc_widget.remove_view(subview)
 
 
@@ -458,8 +516,12 @@ class ChannelView(View):
     userlist: _UserList  # no idea why this is needed to avoid mypy error
 
     def __init__(self, server_view: ServerView, channel_name: str, nicks: list[str]):
-        super().__init__(server_view.irc_widget, channel_name, parent_view_id=server_view.view_id)
-        self.irc_widget.view_selector.item(self.view_id, image=server_view.irc_widget.channel_image)
+        super().__init__(
+            server_view.irc_widget, channel_name, parent_view_id=server_view.view_id
+        )
+        self.irc_widget.view_selector.item(
+            self.view_id, image=server_view.irc_widget.channel_image
+        )
         self.userlist = _UserList(server_view.irc_widget)
         self.userlist.set_nicks(nicks)
 
@@ -477,7 +539,9 @@ class ChannelView(View):
         self.userlist.treeview.destroy()
 
     def on_privmsg(self, sender: str, message: str, pinged: bool = False) -> None:
-        sender, chunks = _parse_privmsg(sender, message, self.server_view.core.nick, self.userlist.get_nicks())
+        sender, chunks = _parse_privmsg(
+            sender, message, self.server_view.core.nick, self.userlist.get_nicks()
+        )
         self.add_message(sender, *chunks, pinged=pinged)
 
     def on_join(self, nick: str) -> None:
@@ -502,7 +566,9 @@ class ChannelView(View):
             show_in_gui=self.server_view.should_show_join_leave_message(nick),
         )
 
-    def on_kick(self, channel: str, kicker: str, kicked_nick: str, reason: str | None) -> None:
+    def on_kick(
+        self, channel: str, kicker: str, kicked_nick: str, reason: str | None
+    ) -> None:
         self.userlist.remove_user(kicked_nick)
         if reason is None:
             reason = ""
@@ -556,14 +622,22 @@ class ChannelView(View):
             nick_tag = "self-nick"
         else:
             nick_tag = "other-nick"
-        self.add_message("*", (nick, [nick_tag]), (f" changed the topic of {self.channel_name}: {topic}", []))
+        self.add_message(
+            "*",
+            (nick, [nick_tag]),
+            (f" changed the topic of {self.channel_name}: {topic}", []),
+        )
 
 
 # PM = private messages, also known as DM = direct messages
 class PMView(View):
     def __init__(self, server_view: ServerView, nick: str):
-        super().__init__(server_view.irc_widget, nick, parent_view_id=server_view.view_id)
-        self.irc_widget.view_selector.item(self.view_id, image=server_view.irc_widget.pm_image)
+        super().__init__(
+            server_view.irc_widget, nick, parent_view_id=server_view.view_id
+        )
+        self.irc_widget.view_selector.item(
+            self.view_id, image=server_view.irc_widget.pm_image
+        )
 
     # Same as view_name, but only PM views have this attribute
     @property
@@ -575,7 +649,10 @@ class PMView(View):
 
     def on_privmsg(self, sender: str, message: str) -> None:
         sender, chunks = _parse_privmsg(
-            sender, message, self.server_view.core.nick, [self.server_view.core.nick, self.nick_of_other_user]
+            sender,
+            message,
+            self.server_view.core.nick,
+            [self.server_view.core.nick, self.nick_of_other_user],
         )
         self.add_message(sender, *chunks)
 
