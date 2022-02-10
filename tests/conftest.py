ca73d060f5b1c91e88f91d432aee25af0ad21eec
Akuli <akuviljanen17@gmail.com>
2022-02-10 13:00:32 +0200

    add debug print to figure out why tests freeze (#223)

--- tests/conftest.py
+++ tests/conftest.py
@@ -161,7 +161,7 @@ def alice_and_bob(irc_server, root_window, wait_until, mocker, irc_widgets_dict)
         for irc_widget in irc_widgets_dict.values():
             for server_view in irc_widget.get_server_views():
                 server_view.core.quit()
-                server_view.core.wait_for_threads_to_stop()
+                server_view.core.wait_for_threads_to_stop(verbose=True)
             # On windows, we need to wait until log files are closed before removing them
             wait_until(lambda: not irc_widget.winfo_exists())
             shutil.rmtree(irc_widget.log_manager.log_dir)
