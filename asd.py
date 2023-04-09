eb33a6f93272b27e482ff65f6ad232dbfe0abcaf
Akuli <akuviljanen17@gmail.com>
2023-04-09 12:09:21 +0300

    Revert "oops"
    This reverts commit 9452af4a46d7c68c8945b9f632f06d3bdfdba41a.

--- asd.py
+++
@@ -1,4 +0,0 @@
-import platformdirs, appdirs
-print("appdirs:", appdirs.user_config_dir("mantaray", "Akuli"))
-print("platformdirs:", platformdirs.user_config_dir("mantaray", "Akuli"))
-assert appdirs.user_config_dir("mantaray", "Akuli") == platformdirs.user_config_dir("mantaray", "Akuli")
