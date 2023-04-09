a714bad2d2c07d436a602f9ddc01d3216a2c092d
Akuli <akuviljanen17@gmail.com>
2023-04-09 13:42:08 +0300

    improve

--- mantaray/commands.py
+++ mantaray/commands.py
@@ -77,9 +77,9 @@ def handle_command(view: View, core: IrcCore, entry_text: str, history_id: int) 
                 " Are you sure you want to do it?"
             ),
         )
+        view.irc_widget.entry.focus()
         if not result:
             return
-        view.irc_widget.entry.focus()
 
     for line in lines:
         _send_privmsg(view, core, line, history_id=history_id)
