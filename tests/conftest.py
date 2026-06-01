cdb1e38681f29bb42216fc0e903a89725fc37d75
Akuli <akuviljanen17@gmail.com>
2026-06-01 22:51:16 +0300

    oh

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,13 +18,13 @@ from mantaray import config, gui
 os.environ.setdefault("IRC_SERVER", "mantatail")
 
 
-# To speed up CI, it's possible to set SHARD to e.g. "1/3", "2/3", "3/3" to run
-# the tests in multiple parts.
+# To speed up CI, it's possible to set MANTASHARD to e.g. "1/3", "2/3", "3/3"
+# to run the tests in multiple parts.
 #
 # Based on: https://github.com/AdamGleave/pytest-shard/blob/64610a08dac6b0511b6d51cf895d0e1040d162ad/pytest_shard/pytest_shard.py#L43-L63
 # The code linked above is: Copyright 2019 Adam Gleave
 def pytest_collection_modifyitems(config, items):
-    shard_index, num_shards = map(int, os.environ.get("SHARD", "1/1").split("/"))
+    shard_index, num_shards = map(int, os.environ.get("MANTASHARD", "1/1").split("/"))
     shard_index -= 1
     items[:] = [
         item
